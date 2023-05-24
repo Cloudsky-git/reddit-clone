93ba1ece5fc0b0c0548f4b069f31278fefd6c020
Cloud <maniuuur@gmail.com>
2023-05-24 12:20:03 +0200

    Posts model with basic views, display and interactions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :accounts
-
-  resources :communities
+  resources :communities do
+    resources :posts
+  end 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   root to: "public#index"
