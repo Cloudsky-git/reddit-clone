8b6888e451dc86a2bf3b33f7b5d343cf0cbbdbc7
Cloud <maniuuur@gmail.com>
2023-05-25 15:55:08 +0200

    Layouts, communities, user/profile details with updatable settings

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import 'bootstrap'
+import "jquery"
+import "jquery_ujs"
+import "popper"
+
--- config/importmap.rb
+++ config/importmap.rb
@@ -2,3 +2,6 @@
 
 pin "application", preload: true
 pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.1.3/dist/js/bootstrap.esm.js"
+pin "jquery", to: "https://code.jquery.com/jquery-3.6.0.min.js", preload: true
+pin "jquery_ujs", to: "jquery_ujs.js", preload: true
+pin "popper", to: "popper.js", preload: true
